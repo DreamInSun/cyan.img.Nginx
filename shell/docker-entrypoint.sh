3af037a9a157642a31ec3f4cc95debdb9ad0bf8a
DreamInSun <yancy_chen@hotmail.com>
2016-03-22 12:59:51 +0800

    Signed-off-by: DreamInSun <yancy_chen@hotmail.com>

--- shell/docker-entrypoint.sh
+++ shell/docker-entrypoint.sh
@@ -3,6 +3,8 @@ echo ========== Set Up Environment ==========
 echo NGINX_HOME is $NGINX_HOME
 echo NGINX_DATA is $NGINX_DATA
 
+echo ========== Set Up Environment ==========
+mkdir -p $NGINX_HOME/log
 
 echo ========== Start Nginx ==========
 nginx -g 'daemon off;'
